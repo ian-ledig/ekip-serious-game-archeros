32c55c07a01fe9de699061308c49407666797df6
msd11 <msd11@autistici.org>
2020-10-06 11:23:25 +0200

    Add MapEventHandler and move event inside

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,74 +14,20 @@ import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
-    private static final double MIN_FLICK_PIXELS = 5;
-    private static final double FLICK_MOVEMENT = 5;
-
-    private enum Direction {
-        LEFT, RIGHT, UP, DOWN
-    }
-
-    private double startDragX;
-    private double startDragY;
-    private double lastXPosition;
-    private double lastYPosition;
-    private Direction lastFlickDirection = null;
-
     @Override
     public void start(Stage primaryStage) throws Exception {
         Group root = new Group();
         Scene scene = new Scene(root, 850, 500);
         scene.setFill(Color.TRANSPARENT);
+
+        // Creation of map page
         Map newMap = new Map();
         Group map = newMap.getGroup();
         root.getChildren().add(map);
 
-        map.setOnMousePressed((e) -> {
-            startDragX = e.getSceneX();
-            startDragY = e.getSceneY();
-            lastXPosition = e.getSceneX();
-            lastYPosition = e.getSceneY();
-        });
-
-        map.setOnMouseDragged((e) -> {
-            double currentX = e.getSceneX();
-            double currentY = e.getSceneY();
-
-            Direction direction;
-            // Detect as "right flick" if the previous flick direction is not right, and the
-            // dragged pixels is more than 10
-            if (lastFlickDirection != Direction.RIGHT && currentX - lastXPosition > MIN_FLICK_PIXELS) {
-                direction = Direction.RIGHT;
-                map.setTranslateX(map.getTranslateX() + FLICK_MOVEMENT);
-                lastXPosition = currentX;
-            }
-
-            // Detect as "left flick" if the previous flick direction is not left, and the
-            // dragged pixels is more than -10
-            else if (lastFlickDirection != Direction.LEFT && currentX - lastXPosition < -MIN_FLICK_PIXELS) {
-                direction = Direction.LEFT;
-                map.setTranslateX(map.getTranslateX() - FLICK_MOVEMENT);
-                lastXPosition = currentX;
-            }
-
-            else if (lastFlickDirection != Direction.DOWN && currentY - lastYPosition > MIN_FLICK_PIXELS) {
-                direction = Direction.DOWN;
-                map.setTranslateY(map.getTranslateY() + FLICK_MOVEMENT);
-                lastYPosition = currentY;
-            }
-
-            else if (lastFlickDirection != Direction.UP && currentY - lastYPosition < -MIN_FLICK_PIXELS) {
-                direction = Direction.UP;
-                map.setTranslateY(map.getTranslateY() - FLICK_MOVEMENT);
-                lastYPosition = currentY;
-            }
-            // event.setDragDetect(true);
-            // newMap.move(event.getX(), event.getY());
-        });
-
-        map.setOnMouseReleased(e -> {
-            lastFlickDirection = null;
-        });
+        // Map Event Handler (temporaire)
+        MapEventHandler mev = new MapEventHandler();
+        mev.addMapEventHandler(map);
 
         primaryStage.setTitle("L'Ekip");
         primaryStage.setScene(scene);
