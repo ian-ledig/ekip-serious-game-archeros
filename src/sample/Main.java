876953fd879847cfe8de4c21a45fbdb98ed003f5
Pacito2 <ian.ledigfr@gmail.com>
2020-10-08 14:30:45 +0200

    Fix workspace

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,22 +1,10 @@
 package sample;
 
-import javafx.util.Duration;
-
-import javafx.animation.*;
-import javafx.animation.KeyValue;
-import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.skin.TextInputControlSkin.Direction;
-import javafx.scene.input.MouseDragEvent;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
