36cc7eb4e159ce4b78a8f2338424b3ac630b4180
Enes <ozbele@outlook.fr>
2020-12-04 15:28:01 +0100

    Add Function pause()

--- src/fr/lekip/pages/PageMining.java
+++ src/fr/lekip/pages/PageMining.java
@@ -1,5 +1,6 @@
 package fr.lekip.pages;
 
+import fr.lekip.Main;
 import fr.lekip.components.GameGroup;
 import fr.lekip.components.GameImage;
 import fr.lekip.components.GamePlayer;
@@ -7,10 +8,14 @@ import fr.lekip.inputs.PlayerMovementsEventHandler;
 import fr.lekip.utils.GroundType;
 import fr.lekip.utils.Item;
 import fr.lekip.utils.SkyboxType;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 
 import java.io.File;
@@ -35,6 +40,11 @@ public class PageMining extends GameGroup {
 
     private Label score;
     private int itemFoundCount = 0;
+
+    private Button btnPause = new Button("II");
+    private Button btnResume = new Button("Reprendre");
+    private Button btnRestart = new Button("Recommencer");
+    private Button btnAbandon = new Button("Abandonner");
 
     public PageMining(SkyboxType skyboxType, List<GroundType> groundTypes, List<Item> items)
             throws FileNotFoundException {
@@ -86,7 +96,7 @@ public class PageMining extends GameGroup {
                     itemFoundCount++;
                     score.setText(SCORE_BASE_TEXT + itemFoundCount + "/" + groundItems.size());
 
-                    if(isEnd()){
+                    if (isEnd()) {
                         // TO DO : End the party
                     }
                 });
@@ -147,29 +157,31 @@ public class PageMining extends GameGroup {
 
         loadEnergyBar();
         loadLabels();
+        pause();
 
         // Add player movements event handler
         addEventHandler(PlayerMovementsEventHandler.class);
     }
 
-    public int tryAPos(double objectSize, boolean isXChecker){
+    public int tryAPos(double objectSize, boolean isXChecker) {
         int result = 0;
         boolean correctPos = false;
 
-        while(!correctPos){
+        while (!correctPos) {
             correctPos = true;
-            result = isXChecker ?
-                    (int) ((Math.random() * (GROUND_BLOCKS_LINE_NUMBER * GroundType.GROUND_SIZE - Item.MAX_ITEM_SIZE * 2 - Item.MAX_ITEM_SIZE * 2)) + Item.MAX_ITEM_SIZE) :
-                    (int) ((Math.random() * (GROUND_BLOCKS_ROW_NUMBER * GroundType.GROUND_SIZE - Item.MAX_ITEM_SIZE * 2)) + 262);
+            result = isXChecker
+                    ? (int) ((Math.random() * (GROUND_BLOCKS_LINE_NUMBER * GroundType.GROUND_SIZE
+                            - Item.MAX_ITEM_SIZE * 2 - Item.MAX_ITEM_SIZE * 2)) + Item.MAX_ITEM_SIZE)
+                    : (int) ((Math.random()
+                            * (GROUND_BLOCKS_ROW_NUMBER * GroundType.GROUND_SIZE - Item.MAX_ITEM_SIZE * 2)) + 262);
 
-            if(groundItems.isEmpty())
+            if (groundItems.isEmpty())
                 correctPos = true;
             else {
-                for(GameImage imgItem : groundItems){
-                    if(
-                            result + objectSize >= (isXChecker ? imgItem.getXImage() : imgItem.getYImage())  &&
-                            result <= (isXChecker ? imgItem.getXImage() : imgItem.getYImage()) + imgItem.getFitWidth()
-                    ){
+                for (GameImage imgItem : groundItems) {
+                    if (result + objectSize >= (isXChecker ? imgItem.getXImage() : imgItem.getYImage())
+                            && result <= (isXChecker ? imgItem.getXImage() : imgItem.getYImage())
+                                    + imgItem.getFitWidth()) {
                         correctPos = false;
                         break;
                     }
@@ -178,11 +190,9 @@ public class PageMining extends GameGroup {
         }
         return result;
     }
-    
-    public boolean isEnd(){
-        return
-                itemFoundCount == groundItems.size() - 1 ||
-                energyBar.getProgress() <= 0;
+
+    public boolean isEnd() {
+        return itemFoundCount == groundItems.size() - 1 || energyBar.getProgress() <= 0;
     }
 
     public List<GameImage> getGroundItems() {
@@ -245,12 +255,82 @@ public class PageMining extends GameGroup {
 
     public void loadLabels() throws FileNotFoundException {
         score = new Label(SCORE_BASE_TEXT + itemFoundCount + "/" + groundItems.size());
-        score.setFont(Font.loadFont(new FileInputStream(new File("src/assets/font/bebas_neue/BebasNeue-Regular.ttf")), 27.0));
+        score.setFont(
+                Font.loadFont(new FileInputStream(new File("src/assets/font/bebas_neue/BebasNeue-Regular.ttf")), 27.0));
         HBox hbox = new HBox(20);
         hbox.setTranslateX(1300);
         hbox.setTranslateY(80);
         hbox.setSpacing(5);
         hbox.getChildren().add(score);
         add(hbox);
+    }
+
+    public void pause() {
+        btnPause.setMinSize(40, 40);
+        HBox hbox = new HBox(20);
+        hbox.setTranslateX(20);
+        hbox.setTranslateY(40);
+        hbox.getChildren().add(btnPause);
+        add(hbox);
+
+        btnPause.setOnAction((EventHandler<ActionEvent>) new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+
+                VBox vbox = new VBox();
+                vbox.setPrefWidth(150);
+                btnResume.setMinHeight(30);
+                btnRestart.setMinHeight(30);
+                btnAbandon.setMinHeight(30);
+                btnResume.setMinWidth(vbox.getPrefWidth());
+                btnRestart.setMinWidth(vbox.getPrefWidth());
+                btnAbandon.setMinWidth(vbox.getPrefWidth());
+                vbox.setTranslateX(650);
+                vbox.setTranslateY(300);
+                vbox.setSpacing(25);
+                vbox.getChildren().addAll(btnResume, btnRestart, btnAbandon);
+
+                add(vbox);
+
+                btnResume.setVisible(true);
+                btnRestart.setVisible(true);
+                btnAbandon.setVisible(true);
+            }
+        });
+
+        btnResume.setOnAction((EventHandler<ActionEvent>) new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+               //Retirer pause
+               btnResume.setVisible(false);
+               btnRestart.setVisible(false);
+               btnAbandon.setVisible(false);
+            }
+        });
+
+        btnRestart.setOnAction((EventHandler<ActionEvent>) new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+               //Recommencer
+            }
+        });
+
+        btnAbandon.setOnAction((EventHandler<ActionEvent>) new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    Main.setShowedPage(new PageMap());
+                } catch (FileNotFoundException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        
     }
 }
